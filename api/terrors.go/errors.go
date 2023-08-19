35ea233470bfdc4a148d963f8c1a50a86705d9a9
LMF709268224 <709268224@qq.com>
2023-08-19 17:12:57 +0800

    errors

---
+++ api/terrors.go/errors.go
@@ -0,0 +1,15 @@
+package terrors
+
+const (
+	// common
+	NotFound           = iota + 10000
+	DatabaseError      // 数据库错误
+	ParametersAreWrong // 参数错误
+	CidToHashFiled     // cid转hash出错
+	EncodingError      // 编码错误
+	DecodingError      // 解码错误
+	UserNotFound       // 找不到用户
+
+	Success = 0
+	Unknown = -1
+)
