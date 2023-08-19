571379add84fb369d7f84021931db3cf57781e85
LMF709268224 <709268224@qq.com>
2023-08-19 19:46:15 +0800

    user map lock

--- node/user/user.go
+++ node/user/user.go
@@ -3,6 +3,7 @@ package user
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 
 	"github.com/LMF709268224/titan-vps/api/types"
@@ -11,34 +12,39 @@ import (
 
 // Manager is the node manager responsible for managing the online nodes
 type Manager struct {
-	User map[string]*types.UserInfoTmp
+	user     map[string]*types.UserInfoTmp
+	userLock *sync.Mutex
 }
 
 // NewManager creates a new instance of the node manager
 func NewManager() (*Manager, error) {
 	mgr := make(map[string]*types.UserInfoTmp)
 	manager := &Manager{
-		User: mgr,
+		user:     mgr,
+		userLock: &sync.Mutex{},
 	}
 	return manager, nil
 }
 
 func (m *Manager) GenerateSignCode(key string) string {
+	m.userLock.Lock()
+	defer m.userLock.Unlock()
+
 	randNew := rand.New(rand.NewSource(time.Now().UnixNano()))
 	Code := "Vps(" + fmt.Sprintf("%06d", randNew.Intn(1000000)) + ")"
-	v, ok := m.User[key]
+	v, ok := m.user[key]
 	if ok {
 		v.UserLogin.SignCode = Code
 	} else {
-		m.User[key] = &types.UserInfoTmp{}
-		m.User[key].UserLogin.SignCode = Code
-		m.User[key].UserLogin.UserId = key
+		m.user[key] = &types.UserInfoTmp{}
+		m.user[key].UserLogin.SignCode = Code
+		m.user[key].UserLogin.UserId = key
 	}
 	return Code
 }
 
 func (m *Manager) GetSignCode(key string) (string, error) {
-	v, ok := m.User[key]
+	v, ok := m.user[key]
 	fmt.Println(ok)
 	fmt.Println(v)
 	if ok && v.UserLogin.SignCode != "" {
