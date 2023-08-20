1557aef335a9e32c7387bb394ffe13ec86a4264e
cribug <88486360@qq.com>
2023-08-20 17:19:41 +0800

    update api

--- node/orders/manager.go
+++ node/orders/manager.go
@@ -355,8 +355,8 @@ func (m *Manager) CronGetInstanceDefaultInfo() {
 	task := func() {
 		m.UpdateInstanceDefaultInfo(ctx)
 	}
-	//spec := "0 0 1,13 * * ?"
-	spec := "*/60 * * * * ?"
+	spec := "0 0 1,13 * * ?"
+	//spec := "*/60 * * * * ?"
 	crontab.AddFunc(spec, task)
 	crontab.Start()
 	fmt.Println("start")
